2d6fbab4fd448a4cafa372e66973e19662b95d1b
liangdongfang <781098813@qq.com>
2020-06-16 19:45:37 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,15 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开本页面，以保证浏览效果')
+  const img = document.createElement('img')
+  img.src = "./qrcode.png"
+  document.body.appendChild(img)
+  img.style.position = 'fixed'
+  img.style.left = '50%'
+  img.style.top = '50%'
+  img.style.transform = 'translate(-50% , -50%)'
+  img.style.boxShadow = '0 0 18px #c4c4c4'
+}
